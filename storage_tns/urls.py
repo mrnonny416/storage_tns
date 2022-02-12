aad4b9a831906ea0673391c0e8bbbf0e7418bff8
ARTA CHERMUE <68236244+Nightke-NK@users.noreply.github.com>
2022-02-12 15:07:58 +0700

    Update urls.py
    edit urls

--- storage_tns/urls.py
+++ storage_tns/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import url
 
+def helloworld(request):
+    return HttpResponse("Hello Test ")
+
 urlpatterns = [
-    url('admin/', admin.site.urls),
+    url(r'admin/', admin.site.urls),
+    url(r'^$', helloworld),
 ]
-#test
+
