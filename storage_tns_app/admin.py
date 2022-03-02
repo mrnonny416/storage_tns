3cb7e12ae10efcd49249cb5a9d28658eb549ab16
mrnonny416 <mrnonny416@gmail.com>
2022-03-02 13:23:37 +0700

    edit admin page

--- storage_tns_app/admin.py
+++ storage_tns_app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from . import models
 
+admin.site.site_header = "TNS Storage Admin"
+admin.site.site_title = "ยินดีต้อนรับเข้าสู่ระบบจัดการของ Admin"
+admin.site.index_title = "TNS Storage Manage Admin"
+
 admin.site.register(models.user)
 admin.site.register(models.material)
 admin.site.register(models.storage)
